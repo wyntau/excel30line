9a85f74c3c107ede4ed9eb2350ad7930550ed4e2
Jeremial <jeremial90@gmail.com>
2013-12-10 09:37:37 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+## excel30line
+
+使用30行js代码实现一个excel应用.
+
+原地址: <http://jsfiddle.net/ondras/hYfN3/>
+
+![example](screenshot.png)
---
+++ index.js
@@ -0,0 +1,29 @@
+for (var i=0; i<6; i++) {
+    var row = document.querySelector("table").insertRow(-1);
+    for (var j=0; j<6; j++) {
+        var letter = String.fromCharCode("A".charCodeAt(0)+j-1);
+        row.insertCell(-1).innerHTML = i&&j ? "<input id='"+ letter+i +"'/>" : i||letter;
+    }
+}
+
+var DATA={}, INPUTS=[].slice.call(document.querySelectorAll("input"));
+INPUTS.forEach(function(elm) {
+    elm.onfocus = function(e) {
+        e.target.value = localStorage[e.target.id] || "";
+    };
+    elm.onblur = function(e) {
+        localStorage[e.target.id] = e.target.value;
+        computeAll();
+    };
+    var getter = function() {
+        var value = localStorage[elm.id] || "";
+        if (value.charAt(0) == "=") {
+            with (DATA) return eval(value.substring(1));
+        } else { return isNaN(parseFloat(value)) ? value : parseFloat(value); }
+    };
+    Object.defineProperty(DATA, elm.id, {get:getter});
+    Object.defineProperty(DATA, elm.id.toLowerCase(), {get:getter});
+});
+(window.computeAll = function() {
+    INPUTS.forEach(function(elm) { try { elm.value = DATA[elm.id]; } catch(e) {} });
+})();
